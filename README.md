dc32cd1fecd8a6c1f891bacc275ed2c6cd266a6a
Justin Richards <richards.justin@gmail.com>
2014-08-12 11:30:02 -0400

    Updating readme, adding license, fixing some misc issues.

--- README.md
+++ README.md
@@ -1,9 +1,20 @@
 couchdb-copydoc
 ===============
 
-An interface for replication of single couchdb documents across servers. Written in nodejs/jade.
-
+An interface to couch db's replication engine, to copy single couchdb documents across servers easily. Written in nodejs/jade/foundation.
 
 Note:
 This is still alpha code, and bugs are still awry. Help contribute and help me sqaush those nasty bugs!
+Would like to make this more dynamic in the future to add/save servers and settings through some sort of panel.
+
+Required Values to set:
+===============
+Under copydoc.js set the following values
+1. Set servers and datbases
+2. Set Authentication, if needed
+3. Set User Ctx Values
+
+
+Licensed under GPL v3.0(http://choosealicense.com/licenses/gpl-3.0)
+
 
--- copydoc.js
+++ copydoc.js
@@ -1,100 +1,88 @@
 //copydoc
 //=======
+
+// Required Values
+// 1. Set servers and datbases
+// 2. Set Authentication, if needed
+// 3. Set User Ctx Values
 var request = require('request');
 
 module.exports = {
-	//Set your dev/ref/prod or other commmon servers
-	// TODO: Remove int/ext reference as they don't apply to global usage
+	//List of available servers
 	servers: {
 		"dev":{
-			"int":"",
-			"ext":""
 		},
 		"ref":{
-			"int":"",
-			"ext":""
 		},
 		"prod":{
-			"int":"",
-			"ext":""
 		}
 	},
 	
+	//Databases on the servers to access
 	dbs: {
-		"int":"",
-		"ext":""		
+		"db1":"",
+		"db2":""		
 	}, 
-		
+	
+	
 	copy: function(data){
 	
-
+		//Authentication, Enter your name and password
+		var username = 'MYNAME';
+		var password = 'MYPASSWORD';
+		var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
+		
 		//From Server: Where are we getting this data from?
 		var fromServer = (data.body.cfrom !== '')? data.body.cfrom : this.servers[ data.body.from ][ data.body.serverType ];
 		var fromLocation = ( fromServer + "/" + this.dbs[ data.body.serverType ]).split(' ').join('');
 		
 		//Where are we sending it to
 		var toServer = (data.body.cto !== '')? data.body.cto : this.servers[ data.body.to ][ data.body.serverType ]; 
-		var toLocation = toServer  + '/_replicate';
+		var toLocation = toServer  + '/_replicator';
 		
 		//Target Database
 		var target = this.dbs[ data.body.serverType ];   
-				
-		//Authentication
-		var username = '';
-		var password = '';
-		var auth = 'Basic ' + new Buffer(username + ':' + password).toString('base64');
-								
-		//Setup Auth Headers
-		if(!data.body.noAuth){
-			var headers = {'Content-Type':"application/json", 'Authorization': auth}				
+						
+		//Setup Headers
+		//TODO: Not the best way but works for the time being. 
+		// would be great if it was automated somehow or allowed for custome auth values.
+		if(!data.body.noauth){
+			var headers = {'Content-Type':"application/json", 'Authorization': auth};				
 		}else{
-			var headers = {'Content-Type':"application/json"}	
+			var headers = {'Content-Type':"application/json"};
 		}
+						
+		//Some helpful output
+		console.log(fromLocation);	
+		console.log(target);
+		console.log(toLocation); 
 		
 		//Start Request
-		request({url:fromServer}, function (fError, fResponse, fBody) {
-		  if (!fError && fResponse.statusCode == 200) {
-			  			
-			console.log(fBody);
-			
-			var myObj = {"source":fromLocation, "target": target  , "doc_ids":[data.body.docid]}; 
-			 
-	  	    request(
-	  			{
-	  				method:"post", 
-	  				url:toLocation,	
-	  				headers:headers,
-	  				body:JSON.stringify(myObj)
-
-	  			}, 
-	  			function (tErr, tResponse, tBody){
-	  				console.log(tResponse.request);
-	  	    		if(tErr){
-	  	    			return console.log("upload failed", tErr);
-	  	    		}
-	  				return console.log("You did it!, server gave us", tBody);
-	  	    });
-			
-	  	    request(
-	  			{
-	  				method:"post", 
-	  				url:toLocation,	
-	  				headers:headers,
-	  				body:JSON.stringify(myObj)
-	  			}, 
-	  			function (tErr, tResponse, tBody){
-	  				console.log(tResponse.request);
-	  	    		if(tErr){
-	  	    			return console.log("upload failed", tErr);
-	  	    		}
-	  				return console.log("You did it!, server gave us", tBody);
-	  	    });
+		//Set User_ctx name and roles
+		var myObj = {"source":fromLocation, "target": target  , "doc_ids":[data.body.docid], 
+						"user_ctx": {
+  							"name": "",
+								'roles':['']
+  	  				}
+			}; 
 		
-			
-		  }
+		request({
+	  				method:"post", 
+	  				url:toLocation,	
+	  				headers:headers,
+	  				body:JSON.stringify(myObj)
+	  			}, 
+					function (fError, fResponse, fBody) {
+  	    		if(fError){
+  	    			return console.log("upload failed", fError);			
+  	    		}
+					  if (!fError && fResponse.statusCode == 200) {
+							console.log(fBody.body); 
+							return console.log("You did it!, server gave us", fResponse);					
+					  }
+						console.log(fResponse); 
 		})
-		
-
+	
 		
 		return data.body;
 	}
